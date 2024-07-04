ca2d61432a36834cc8fe85e9ebd378e9fa7b84d4
Keerthi-Jangam <keerthi.jangam@everest.engineering>
2024-07-04 10:54:55 +0530

    Add tsconfig.json file

--- db_connection/index.ts
+++ db_connection/index.ts
@@ -1,37 +1,83 @@
 // const { Sequelize } = require("sequelize");
-import { User } from "../Models/UserDetails";
+import { Book } from "../models/bookModel/book";
+import { Loan } from "../models/loanModel/loan";
+import { Member } from "../models/memberModel/member";
+import { Author } from "../models/authorModel/author";
+import { Reservation } from "../models/resevationModel/reservation";
 import { sequelize } from "./config";
+import { insertBooksData } from "../models/bookModel/insertData";
+import { insertAuthorsData } from "../models/authorModel/insertData";
+import { insertLoansData } from "../models/loanModel/insertData";
+import { insertMembersData } from "../models/memberModel/insertData";
+import { insertReservationData } from "../models/resevationModel/insertData";
+// import { insertMemberData } from "../models/memberModel/insertData";
+// const sequelize = new Sequelize("school_db", "postgres", "1405", {
+//   host: "localhost",
+//   dialect: "postgres",
+//   port: 5433,
+// });
 
-// const sequelize = new Sequelize(
-//   "KeerthiFirstDatabase",
-//   "postgres",
-//   "Rachubai123*",
-//   {
-//     host: "localhost",
-//     dialect: "postgres",
-//     port: 5432,
-//   }
-// );
-const connectionFunction = async () => {
+const Connection = async () => {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
-    await User.sync({force:true})
-    console.log("table created");
+    await Author.sync()
+      .then(() => {
+        console.log("hiii");
+      })
+      .catch((err: any) => {
+        console.log("bye");
+      });
+    await Book.sync()
+      .then(() => {
+        console.log("hiii");
+      })
+      .catch((err: any) => {
+        console.log("bye");
+      });
+    await Member.sync()
+      .then(() => {
+        console.log("hiii");
+      })
+      .catch((err: any) => {
+        console.log("bye");
+      });
+    await Loan.sync()
+      .then(() => {
+        console.log("hiii");
+      })
+      .catch((err: any) => {
+        console.log("bye");
+      });
+    await Reservation.sync()
+      .then(() => {
+        console.log("hiii");
+      })
+      .catch((err: any) => {
+        console.log("bye");
+      });
+    console.log("sync succesfull");
+    // await Member.book();
+    // await Author.drop();
+
+    insertBooksData();
+    console.log("booksdata inserted successfully");
+
+    insertAuthorsData();
+    console.log("authorsdata inserted successfully");
+
+    insertLoansData();
+    console.log("loansdata inserted successfully");
+
+    insertMembersData();
+    console.log("memberdata inserted successfully");
+
+    // insertReservationData()
+    // console.log("rservationdata inserted successfully");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
 };
 
-// const firstTable = async () => {
-//   try {
-//     await User.sync({ force: true });
-//     console.log("table created");
-//   } catch (error) {
-//     console.error("unable to create table", error);
-//   }
-// };
-
-connectionFunction();
-// firstTable();
+Connection();
 export { sequelize };
